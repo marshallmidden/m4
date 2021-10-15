003a5faa38b14e886c57c7d03cb68c4b02e28382
Marshall Midden <m4@parseclabs.com>
2021-10-14 22:50:16 -0500

    Need more stuff for 7.5 redhat development to make new gcc11.1 and kernel 5.15-rc4

--- music/musicomp2abc/c11.pl
+++ music/musicomp2abc/c11.pl
@@ -8,22 +8,94 @@ use warnings;
 # 2) See if faster/slower notes start (loudness) at same rate.
 #    OVERLAP remain the same over speed changes.
 # 3) See if loudness does decade at same rate -- and OVERLAP. (#1,#2)
+#
 # 4) See if instrument not piano does decade at same rate -- and OVERLAP. (#1,#2)
+#	Massive difference in instrument volumes, start of notes, etc.
 # ----------------------------------------------------------------------------
 my $QN = 480;                           # Length in MIDI of a quarter note.
-my $NOTELTH=$QN;
+# my $QN = 240;                           # Length in MIDI of a quarter note.
+
+# my $NOTE_TIME = 1;	# quarter note
+# my $NOTE_TIME = 2;	# half note
+# my $NOTE_TIME = 8;	# whole note
+my $NOTE_TIME = 16;	# double whole note
+
+my $NOTELTH = $QN * $NOTE_TIME;
 my $MAXVOICES = 1;
 my @NOTES = ( 60, 62, 64, 65 );
+my $NUM_SEQ = 4;
+#++ my $NUM_SEQS = 3;
+my $NUM_SEQS = 1;
+my $early = $NOTELTH / 2;
+
+# . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+my $instrument = 0;			# Acoustic Grand Piano
+my $OVERLAP = 10;		# 0 - piano
+my $cc11_start = 80;		# quarter note after quarter note.
+my $cc11_stop = 127;		# quarter note
+
+# . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+# DB scale for double whole note:
+# Starting in seconds:	?0.00195		?4.44687		?8.89204		?13.33562
+#  Ramp to up seconds:	?0.1		?4.61150		?9.04170		?13.47447
+#   Attack Maximum DB:  ?-12db		?-12db		?-12db		?-12db
+#   Decay key release:	?2.22846		?6.62474		?11.11395	?15.55621
+#        DB @ release:	?-28db		?-37db		?-35db		?-35db
+# Stopping in seconds:	?2.61016		?6.96472		?11.42549	?15.89224
+# . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+# DB scale for whole note:
+# Starting in seconds:	0.00195		4.44687		8.89204		13.33562
+#  Ramp to up seconds:	0.1		4.61150		9.04170		13.47447
+#   Attack Maximum DB:  -12db		-12db		-12db		-12db
+#   Decay key release:	2.22846		6.62474		11.11395	15.55621
+#        DB @ release:	-28db		-37db		-35db		-35db
+# Stopping in seconds:	2.61016		6.96472		11.42549	15.89224
+# . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+# DB scale for half note (pan full left).
+# Starting in seconds:	0.00195		2.22501		4.44687		6.66860
+#  Ramp to up seconds:	0.1		2.38963		4.59653		6.73302
+#   Attack Maximum DB:  -12db		-11db		-11db		-11db
+#   Decay key release:	1.15612		3.32156		5.55082		7.77909
+#        DB @ release:	-25db		-25db		-25db		-25db
+# Stopping in seconds:	1.57206		3.71238		5.90725		8.19980
+# . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
+# my $instrument = 40;			# Violin
+# my $instrument = 41;			# Viola
+# my $instrument = 42;			# Cello
+# my $instrument = 43;			# Contrabass
+# my $instrument = 47;			# Timpani
+# my $instrument = 57;			# Trombone
+# my $instrument = 60;			# French Horn
+# my $instrument = 68;			# Oboe
+# my $instrument = 70;			# Bassoon
+# my $instrument = 71;			# Clarinet
+# my $instrument = 72;			# Flute
+
+#     'Violin'.lower() : 40,
+#     'Viola'.lower() : 41,
+#     'Cello'.lower() : 42,
+#     'Contrabass'.lower() : 43,
+#     'Tremolo Strings'.lower() : 44,
+#     'Pizzicato Strings'.lower() : 45,
+#     'Timpani'.lower() : 47,
+#     'Trombone'.lower() : 57,
+#     'French Horn'.lower() : 60,
+#     'Oboe'.lower() : 68,
+#     'Bassoon'.lower() : 70,
+#     'Clarinet'.lower() : 71,
+#     'Flute'.lower() : 73,
+
 
 #-- my $OVERLAP = 80;
 #-- my $OVERLAP = 60;
 #-- my $OVERLAP = 40;
 #-- my $OVERLAP = 30;
 #-- my $OVERLAP = 20;
-my $OVERLAP = 10;
+#-- my $OVERLAP = 10;
 
 # my $ENDTRACKS = ($NOTELTH) * ($MAXVOICES) * 8;
-my $ENDTRACKS = ($NOTELTH) * ($MAXVOICES) * 12;
+my $ENDTRACKS = ($NOTELTH) * ($MAXVOICES) * ($NUM_SEQ * $NUM_SEQS);
 # ----------------------------------------------------------------------------
 sub print_header($$)
 {
@@ -67,11 +139,13 @@ sub print_track($$$$$$$$)
     my ($track, $chan, $inst, $rev, $start, $stop, $note, $pan) = @_;
     printf STDOUT "%d, %d, Program_c, %d, %d\n", $track, $start, $chan, $inst;
     printf STDOUT "%d, %d, Control_c, %d, 121, 0\n", $track, $start, $chan;
-    printf STDOUT "%d, %d, Control_c, %d, 64, 0\n", $track, $start, $chan;
+#--    printf STDOUT "%d, %d, Control_c, %d, 64, 0\n", $track, $start, $chan;
+    printf STDOUT "%d, %d, Control_c, %d, 0, 0\n", $track, $start, $chan;
 #  91   Reverb Level - Affects: this is usually the reverb or delay level.
     printf STDOUT "%d, %d, Control_c, %d, 91, %d\n", $track, $start, $chan, $rev;  # No reverb.
 #  10   Pan position   64 is center, 0 is hard left, and 127 is hard right.
-    printf STDOUT "%d, %d, Control_c, %d, 10, %d\n", $track, $start, $chan, $pan;  # where between stereo l->r.
+#--    printf STDOUT "%d, %d, Control_c, %d, 10, %d\n", $track, $start, $chan, $pan;  # where between stereo l->r.
+    printf STDOUT "%d, %d, Control_c, %d, 0, %d\n", $track, $start, $chan, $pan;  # where between stereo l->r.
 #   7   Volume   maximum is 127 and off.
     printf STDOUT "%d, %d, Control_c, %d, 7, 127\n", $track, $start, $chan;     # Full volume.
 }   # End of print_track
@@ -95,9 +169,10 @@ sub print_legato($$$$$$)
     my $thing = $cc11_start;
     print_expression($track, $start, $chan, $cc11_start);
     my $x = $cc11_stop - $cc11_start;
+    my $d = $cc11_stop;
     for (my $i = 0; $i < $notelth; $i++)
     {
-        my $d = int($cc11_start + (($i / ($notelth - 1)) * ($x + 0.0)));
+        $d = int($cc11_start + (($i / ($notelth - 1)) * $x));
 	if ($i < $OVERLAP)
 	{
 	    $d = $d - int(($OVERLAP - $i) / 4);
@@ -112,13 +187,17 @@ sub print_legato($$$$$$)
 	    print_note_stop($track, $p_stop + $OVERLAP, $chan, $p_note);
 	}
     }
+    if ($d != $cc11_stop)
+    {
+	print_expression($track, $stop, $chan, $cc11_stop);
+    }
 }   # End of print_legato
 
 #-----------------------------------------------------------------------------
 my $track;
 my $chan = 0;
 my $start = 0;
-my $stop = $QN;
+my $stop = $NOTELTH;
 my $note;
 my $prev_note;
 my $prev_stop;
@@ -131,15 +210,15 @@ print_track_end($track, $ENDTRACKS);
 #-----------------------------------------------------------------------------
 $track = $track + 1;
 print_track_start($track, $ENDTRACKS);
-print_track($track, $chan, 0, 0, $start, $stop, $note, 64);
+print_track($track, $chan, $instrument, 0, $start, $stop, $note, 64);
 
 #-----------------------------------------------------------------------------
 # Do normal first.
-for (my $i = 0; $i < 4; $i++)
+for (my $i = 0; $i < $NUM_SEQ; $i++)
 {
     $note = $NOTES[$i];;
     print_note_start($track, $start, $chan, $note);
-    print_note_stop($track, $stop, $chan, $note);
+    print_note_stop($track, $stop - $early, $chan, $note);
     $note = $note + 2;
     $start = $start + $NOTELTH;
     $stop = $stop + $NOTELTH;
@@ -149,30 +228,30 @@ $prev_note = -1;
 $prev_stop = -1;
 
 #-----------------------------------------------------------------------------
-# Do legato second.
-for (my $i = 0; $i < 4; $i++)
-{
-    $note = $NOTES[$i];;
-    print_note_start($track, $start, $chan, $note);
-    print_legato($track, $start, $stop, $chan, $prev_note, $prev_stop);
-    $prev_note = $note;
-    $prev_stop = $stop;
-    $note = $note + 2;
-    $start = $start + $NOTELTH;
-    $stop = $stop + $NOTELTH;
-}
-print_note_stop($track, $prev_stop, $chan, $prev_note);
-#-----------------------------------------------------------------------------
-# Do third normal again.
-for (my $i = 0; $i < 4; $i++)
-{
-    $note = $NOTES[$i];;
-    print_note_start($track, $start, $chan, $note);
-    print_note_stop($track, $stop, $chan, $note);
-    $note = $note + 2;
-    $start = $start + $NOTELTH;
-    $stop = $stop + $NOTELTH;
-}
+#++ # Do legato second.
+#++ for (my $i = 0; $i < $NUM_SEQ; $i++)
+#++ {
+#++     $note = $NOTES[$i];;
+#++     print_note_start($track, $start, $chan, $note);
+#++     print_legato($track, $start, $stop - $early, $chan, $prev_note, $prev_stop);
+#++     $prev_note = $note;
+#++     $prev_stop = $stop;
+#++     $note = $note + 2;
+#++     $start = $start + $NOTELTH;
+#++     $stop = $stop + $NOTELTH;
+#++ }
+#++ print_note_stop($track, $prev_stop, $chan, $prev_note);
+#++ #-----------------------------------------------------------------------------
+#++ # Do third normal again.
+#++ for (my $i = 0; $i < $NUM_SEQ; $i++)
+#++ {
+#++     $note = $NOTES[$i];;
+#++     print_note_start($track, $start, $chan, $note);
+#++     print_note_stop($track, $stop - $early, $chan, $note);
+#++     $note = $note + 2;
+#++     $start = $start + $NOTELTH;
+#++     $stop = $stop + $NOTELTH;
+#++ }
 #-----------------------------------------------------------------------------
 
 print_track_end($track, $ENDTRACKS);
